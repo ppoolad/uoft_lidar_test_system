4ff972970c76a467ac809922968e9d6596f61de7
Pooya <ppoolad@outlook.com>
2024-12-06 19:51:27 +0000

    done some dsp tests

--- src/tdc_monitor/tdc_monitor.c
+++ src/tdc_monitor/tdc_monitor.c
@@ -2,7 +2,7 @@
  * @file tdc_test.c
  * @author Pooya Poolad ppoolad@outlook.com
  *      based on the driver by Jason Gutel jason.gutel@gmail.com
- * resets DSP and starts recording, stores them into a file
+ * resets TDC and starts recording, stores them into a file
  * 
  **/
 
@@ -25,7 +25,7 @@
 #include <time.h>
 #include <poll.h>
 #include <sys/ioctl.h>
-#include <math.h>
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <gpiod.h>
@@ -35,14 +35,7 @@
 #include "asic_control.h"
 #include "conf.h"
 #include "simple_rx.h"
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-#define MAX_VALUE 65535
 /*----------------------------------------------------------------------------
-
  * Internal Definitions
  *----------------------------------------------------------------------------*/
 //#define DEF_DEV_TX "/dev/axis_fifo_0x00000000a0070000"
@@ -52,9 +45,7 @@
 #define DEBUG_PRINT(fmt, args...) printf("DEBUG %s:%d(): " fmt, \
         __func__, __LINE__, ##args)
 
-#ifndef DEBUG 
-    #define DEBUG 0
-#endif
+#define DEBUG 0
 struct thread_data {
     int rc;
 };
@@ -64,7 +55,6 @@ pthread_t read_from_fifo_thread;
 static volatile bool running = true;
 static char _opt_dev_rx[255];
 static int readFifoFd;
-static char output_file[255] = "dsp_values.txt";
 
 static void signal_handler(int signal);
 static void *read_from_fifo_thread_fn(void *data);
@@ -72,7 +62,6 @@ static int process_options(int argc, char * argv[]);
 static void print_opts();
 static void display_help(char * progName);
 static void quit(void);
-void frame_process(char* packets, int size);
 
 //globlas
 //gpio stuff
@@ -80,19 +69,17 @@ struct gpiod_chip *chip;// = gpiod_chip_open_by_name(HPC1_CHIP_NAME);
 struct gpiod_chip *chipled;// = gpiod_chip_open_by_name(LED_CHIP_NAME);
 struct gpiod_line_bulk gpios;
 struct gpiod_line_bulk leds;
-float rolling_avg[6] = {0};
-float alpha_max = 0.001;
-int queue_processed = 0;
+
 //large array to store the values
 char rx_values[MAX_BUF_SIZE_BYTES+16] = {0};
 //file handle to save the values
 FILE *fp;
-FILE *fprnd;
+
 //led values indicating running
 int led_values[8] = {0,0,0,0,0,0,0,1};
 //shift 8 bit array;
 
-int runtime = 40; //run for n secs
+int runtime = 120; //run for n secs
 void shift_array(int *array, int size, int insert){
     //int temp = array[size-1];
     for (int i = size-1; i > 0; i--)
@@ -102,30 +89,6 @@ void shift_array(int *array, int size, int insert){
     //invert of last bit should rotate to 0
     array[0] = insert;
     //array[0] = !array[0];
-}
-
-//generate random number between 0 to 2^16 with mean of n and std of s
-static int rand_initiated = 0;
-int generate_random_number(double mean, double std_dev, int seed) {
-    if (!rand_initiated) {
-        srand(seed);
-        rand_initiated = 1;
-    }
-
-    // Box-Muller transform
-    double u1 = (double)rand() / RAND_MAX;
-    double u2 = (double)rand() / RAND_MAX;
-    double z0 = sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
-    double random_number = mean + z0 * std_dev;
-
-    // Clamp the value between 0 and MAX_VALUE
-    if (random_number < 0) {
-        random_number = 0;
-    } else if (random_number > MAX_VALUE) {
-        random_number = MAX_VALUE;
-    }
-
-    return (int) random_number;
 }
 
 /*----------------------------------------------------------------------------
@@ -178,88 +141,80 @@ int main(int argc, char **argv)
         return -1;
     }
 
+
+
     /*****************/
-    /*   start dsp   */
+    /*   start tdc   */
     /*****************/
 
+    //lets start tdc until it fills the fifo
+    if (!chip) {
+        perror("Open chip failed");
+        exit(EXIT_FAILURE);
+    }
     int hpc1_values[40] = {0};
     printf("setting gpios to 0\n");
     set_gpio_array(chip, &gpios, hpc1_values);
 
-    //reset/unreset dsp
-    dsp_test(chip, &gpios);
-    dsp_reset(chip);
-    dsp_unreset(chip);
+    //unreset tdc
+    tdc_reset(chip);
+    tdc_unreset(chip);
+
+    //create tdc chain
+    unsigned int chain_data[4] = {0};
+    int enables[] = {0,0,0,1,1,0};
+    int offsets[] = {0,0,0,0,0};
+    create_tdc_chain(enables, offsets, chain_data);
+    // configure the chain so only tdc channel 0 is enable
+    //unsigned int chain_data[4] = {0,0,0x1,0xc0000000}; // binary [0000_/01][00/0000|/0000/00|00/0000|/0000/00|00/0000] -> 0x40000000
+    int chain_num_words = 4;
+    int chain_num_bits = 36;
+    int chain_time_out = 100000;
+    configure_chain(chain_data, chain_num_words, chain_num_bits, chain_time_out);
+
+    printf("initiate tdc\n");
+    tdc_test(chip, &gpios);
 
     printf("setting leds to 0x55\n");
     
     set_gpio_array(chipled, &leds, led_values);
     
-    /*****************/
-    /* start threads */
-    /*****************/
-
-    //open file to save values
-    fp = fopen(output_file,"w");
+    //open file
+    fp = fopen("tdc_values_ch3exclusives.txt","w");
     if (fp == NULL) {
         perror("Failed to open file");
         return -1;
     }
-    fprnd = fopen("random_values.txt","w");
-    if (fprnd == NULL) {
-        perror("Failed to open file");
-        return -1;
-    }
+
+    //enable rx 
+    enable_rx();
+    /*****************/
+    /* start threads */
+    /*****************/
+
     /* start thread listening for fifo receive packets */
     rc = pthread_create(&read_from_fifo_thread, NULL, read_from_fifo_thread_fn,
             (void *)NULL);
 
-    // Enable the RX core
-    //set number of serial bits
-    set_rx_nbits(24); //16 data + 8 header
-    //enable rx 
-    enable_rx();
-    dsp_serializer(1,chip);
     /* perform noops */
-    // start the clock
-    clock_t start = clock();
-    int chain_data[4] = {0,0,0,0};
-    while (running && (int)(clock() - start)/CLOCKS_PER_SEC < runtime) {
-        //send random number to DSP
-        int random_number;
-        // generate a uniform random between 0 and 1
-        double u = (double)rand() / RAND_MAX;
-        if (u < 0.5) {
-            // generate a random number between 0 and 2^16 with mean of 0 and std of 1
-            // if (u > 0.05)
-            //     random_number = generate_random_number(8500, 1000, 1);
-            // else
-                random_number = generate_random_number(7000, 800, 1);
-        } else {
-            // generate a random number between 0 and 2^16 with mean of 0 and std of 1
-            random_number = (int)(rand());
-        }
-        fprintf(fprnd,"%d\n",0xFFFF&(random_number));
-        //printf("random number %d\n",0xFFFF&random_number);
-        chain_data[3] = 0xFFFF&(random_number);
-        configure_chain_dsp(chain_data, 4, 16, 1000);
-        // just give it a random number and wait for it to converge
+    while (running) {
+       sleep(runtime); //run for n minutes
+       quit();
     }
-
-    quit();
+    // printf("waiting....\n");
+    // sleep(1); //wait a bit until fifo is full
 
     printf("turn off serializer\n");
-    dsp_serializer(0,chip);
+    tdc_serializer(0,chip);
 
-    printf("reset DSP\n");
-    dsp_reset(chip);
+    printf("reset tdc\n");
+    tdc_reset(chip);
     
     gpiod_chip_close(chip);
     gpiod_chip_close(chipled); //run leds to know it's done
 
     //close file
     fclose(fp);
-    fclose(fprnd);
     cleanup_rx();
     printf("SHUTTING DOWN, wait for thread\n");
     pthread_join(read_from_fifo_thread, NULL);
@@ -284,18 +239,18 @@ static void *read_from_fifo_thread_fn(void *data)
     packets_rx = 0;
     sleep(1);
     while (running) {
-        //wait for fifo to fill up
         while(rx_occupancy < MAX_BUF_SIZE_BYTES && running){
             ioctl(readFifoFd, AXIS_FIFO_GET_RX_OCCUPANCY, &rx_occupancy);
             if(DEBUG) DEBUG_PRINT("rx_occupancy %d\n",rx_occupancy);
             usleep(100);
         }
-
+        // stop serial tx
+        // tdc_serializer(0,chip);
         // stop rx
         disable_rx();
-
-
+        //tdc_reset(chip);
         // empty fifo
+
         while (packets_rx < rx_occupancy)
         {
             bytesFifo = read(readFifoFd, buf, MAX_BUF_SIZE_BYTES);
@@ -313,16 +268,19 @@ static void *read_from_fifo_thread_fn(void *data)
                     for(int nbytes = 0; nbytes < 4; nbytes++){
                         if(DEBUG){
                             if(nbytes == 0)
-                                printf("0x%02x",0);
+                                printf("0x%02x",buf[memidx*4+3-nbytes]);
                             else
                                 printf("%02x",buf[memidx*4+3-nbytes]);
                         }
-                    //dsp data packet is 24 bits
-                    if (nbytes > 0)
-                        rx_values[packets_rx] = buf[memidx*4+3-nbytes];
+                    // if(DEBUG){
+                    //     if(memidx == 0)
+                    //         printf("0x%02x",buf[bytesFifo-1-memidx]);
+                    //     else
+                    //         printf("%02x",buf[bytesFifo-1-memidx]);
+                    // }
+                    rx_values[packets_rx] = buf[memidx*4+3-nbytes]; //buf[bytesFifo-1-memidx];
                     packets_rx = packets_rx + 1;
                     }
-                    //read and print 4 bytes in little endian
                     if(DEBUG)
                         printf("\n\r");
                 }
@@ -331,11 +289,14 @@ static void *read_from_fifo_thread_fn(void *data)
         if(DEBUG)
             DEBUG_PRINT("%d packets read\n\r", packets_rx-4);
         //write to file
-        frame_process(rx_values,packets_rx-4);
         fwrite(rx_values,sizeof(char),packets_rx-4,fp);
         packets_rx = 0;
         rx_occupancy = 0;
-
+        //restart tdc
+        if(DEBUG)
+            DEBUG_PRINT("restart tdc\n\r");
+        //tdc_unreset(chip);
+        //tdc_serializer(1,chip);
         enable_rx();    
         usleep(1000); //necessary to let the fifo fill up        
         shift_array(led_values,8,!led_values[7]);
@@ -344,94 +305,11 @@ static void *read_from_fifo_thread_fn(void *data)
     return (void *)0;
 }
 
-// this needs to be updated for the new data format
-#define DSP_FRAME_SIZE 18
-void frame_process(char* packets, int size){
-    //process the packets
-    if (size % 4 != 0) {
-        printf("Invalid packet size\n");
-        return;
-    }
-
-    int num_packets = size / 4;
-    int packet_index = 0;
-    int correct = 0;
-
-    while (packet_index < num_packets) {
-        int packet = 0;
-        packet = __builtin_bswap32(*(int*)&packets[4*packet_index])&0x00FFFFFF;
-        // Check for Start of Frame (SOF) and End of Frame (EOF)
-        int means[4];
-        int std_devs[4];
-        int weights[4];
-
-        if (packet == 0x00AAAAAA) {
-            //printf("Start of Frame detected\n");
-            // Check for End of Frame (EOF)
-            if (4*packet_index + 4*(DSP_FRAME_SIZE-1) >= num_packets) {
-                //printf("Invalid packet size\n");
-                break;
-            }
-            packet = __builtin_bswap32(*(int*)&packets[4*packet_index + 4*(DSP_FRAME_SIZE-1)]) & 0x00FFFFFF;
-            //printf("Packet[%d]: %08x\n", DSP_FRAME_SIZE-1, packet);
-            if (packet == 0x00AAFFFF) {
-                //printf("End of Frame detected\n");
-                correct = 1;
-                packet_index++;
-                //continue;
-            } else {
-                // Skip to next packet
-                printf("Invalid End of Frame: %08x\n", packet);
-                packet_index++;
-                correct = 0;
-                continue;
-            }
-        } 
-        if (correct == 0){
-            packet_index++;
-            continue;
-        }
-        
-        // parse packet and store into variables
-        //for (int i = 0; i < DSP_FRAME_SIZE; i++) {
-        means[0] = __builtin_bswap32(*(int*)&packets[4*(packet_index-1)+ 4]) & 0x0000FFFF;
-        //printf("Packet[%d]: %08x\n", packet_index-1, means[0]);
-        means[1] = __builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 4+4]) & 0x0000FFFF;
-        means[2] = __builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 8+4]) & 0x0000FFFF;
-        means[3] = __builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 12+4]) & 0x0000FFFF;
-        
-        std_devs[0] = ((__builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 16+4]) & 0x0000FFFF) << 16) | ((__builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 20+4]) & 0x0000FFFF));
-        std_devs[1] = ((__builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 24+4]) & 0x0000FFFF) << 16) | ((__builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 28+4]) & 0x0000FFFF));
-        std_devs[2] = ((__builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 32+4]) & 0x0000FFFF) << 16) | ((__builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 36+4]) & 0x0000FFFF));
-        std_devs[3] = ((__builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 40+4]) & 0x0000FFFF) << 16) | ((__builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 44+4]) & 0x0000FFFF));
-
-        weights[0] = __builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 48+4]) & 0x0000FFFF;
-        weights[1] = __builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 52+4]) & 0x0000FFFF;
-        weights[2] = __builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 56+4]) & 0x0000FFFF;
-        weights[3] = __builtin_bswap32(*(int*)&packets[4*(packet_index-1) + 60+4]) & 0x0000FFFF;
-
-        printf("Means: %d %d %d %d\n", means[0], means[1], means[2], means[3]);
-        printf("Std Devs: %d %d %d %d\n", std_devs[0], std_devs[1], std_devs[2], std_devs[3]);
-        printf("Weights: %.6f %.6f %.6f %.6f\n", weights[0]/65535.0, weights[1]/65535.0, weights[2]/65535.0, weights[3]/65535.0);
-            
-        packet_index += DSP_FRAME_SIZE;
-        correct = 0;
-        //printf("Packet: 0x%08X, Rolling Average: %d\n", packet, rolling_avg);
-    }
-    // print all averages so far in single line
-    //printf("\rCH0: %3f, CH1: %3f, CH2: %3f, CH3: %3f, CH4: %3f, CH5: %3f", rolling_avg[0], rolling_avg[1], rolling_avg[2], rolling_avg[3], rolling_avg[4], rolling_avg[5]);
-
-}
-
 static void signal_handler(int signal)
 {
     switch (signal) {
         case SIGINT:
-            running = false;
-            break;
         case SIGTERM:
-            running = false;
-            break;
         case SIGQUIT:
             running = false;
             break;
@@ -468,12 +346,11 @@ static int process_options(int argc, char * argv[])
 
         for (;;) {
             int option_index = 0;
-            static const char *short_options = "hrno:t:";
+            static const char *short_options = "hrn:t:";
             static const struct option long_options[] = {
                     {"help", no_argument, 0, 'h'},
                     {"devRx", required_argument, 0, 'r'},
                     {"nseconds", required_argument, 0, 'n'},
-                    {"output", required_argument, 0, 'o'},
                     {0,0,0,0},
                     };
 
@@ -498,10 +375,6 @@ static int process_options(int argc, char * argv[])
 
                 case 'n':
                     runtime = atoi(optarg);
-                    break;
-
-                case 'o':
-                    strcpy(output_file, optarg);
                     break;
                     }
             }
